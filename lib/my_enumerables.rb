3d55bb35578780580e0e97236b5964f4ce4ea660
notSean1992 <seandavidmurphy1992@gmail.com>
2025-01-03 20:36:38 +0000

    Update my_enumerables.rb

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,26 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    counter = 0
+    self.my_each do 
+      yield [self[counter], counter]
+      counter += 1
+    end
+  end
+
+  def my_select(&blk)
+    temp_array = Array.new
+    if block_given?
+      self.my_each do |value|
+        if blk.call(value) == true
+          temp_array << value
+        end
+      end
+      return temp_array
+    else
+      return
+    end
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +29,12 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    counter = 0
+    self.size.times do
+      yield self[counter]
+      counter += 1
+    end
+    self
+  end
 end
